4e4378798cb281ace0c7276c2f351ab08201d0fc
gmsvalente <gustavomsvalente@gmail.com>
2022-04-17 15:50:28 -0300

    Create simple app page

--- src/gravie/frontend/render.cljs
+++ src/gravie/frontend/render.cljs
@@ -1,4 +1,14 @@
-(ns gravie.frontend.render)
+(ns gravie.frontend.render
+  (:require [re-frame.core :as rf ]
+            [reagent.dom :as rdom]))
+
+(defn app []
+  [:div
+   [:h1 "Gravie GiantBomb"]])
+
+(defn ^:dev/after-load render []
+  (rdom/render app (.. js/document (getElementById "root"))))
 
 (defn init []
-  (.. js/console (log "INIT")))
+  (println "Init")
+  (render))
