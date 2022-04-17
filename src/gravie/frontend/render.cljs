3021d91c7b9b7942c9511ae9276f87feaf70e5d6
gmsvalente <gustavomsvalente@gmail.com>
2022-04-17 16:04:25 -0300

    Add ui namespace

--- src/gravie/frontend/render.cljs
+++ src/gravie/frontend/render.cljs
@@ -1,13 +1,12 @@
 (ns gravie.frontend.render
   (:require [re-frame.core :as rf ]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            [gravie.frontend.ui :refer [app-ui]]))
 
-(defn app []
-  [:div
-   [:h1 "Gravie GiantBomb"]])
+
 
 (defn ^:dev/after-load render []
-  (rdom/render app (.. js/document (getElementById "root"))))
+  (rdom/render app-ui (.. js/document (getElementById "root"))))
 
 (defn init []
   (println "Init")
---
+++ src/gravie/frontend/ui.cljs
@@ -0,0 +1,6 @@
+(ns gravie.frontend.ui
+  (:require [reagent.core :as r]))
+
+(defn app-ui []
+  [:div [:h1 "Gravie GiantBomb"]])
+
