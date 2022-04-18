0b86d096d43ceef310ac9ebf52f1238aec1e2714
gmsvalente <gustavomsvalente@gmail.com>
2022-04-18 14:48:09 -0300

    Create app routes

---
+++ src/gravie/backend/routes.clj
@@ -0,0 +1,27 @@
+(ns gravie.backend.routes
+  (:require [reitit.ring :as ring ]
+            [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.coercion :as coercion]
+            [reitit.coercion.spec :as rcs]
+            [muuntaja.core :as m]
+            [gravie.backend.handlers :as h]))
+
+(def routes
+  (ring/ring-handler
+   (ring/router
+    [["/" {:name ::root
+           :handler h/land-page}]
+     ["/search/" {:name ::search
+                  :get {:handler h/search}
+                  :middleware [parameters-middleware]}]
+     ["/checkout" {:name ::checkout
+                   :get {:handler h/checkout}}]]
+    {:data {:muuntaja m/instance
+            :middleware [muuntaja/format-middleware]}})
+   (ring/routes
+    (ring/create-file-handler {:path "/"})
+    (ring/create-default-handler
+     {:not-found (fn [_]
+                   {:status 404
+                    :body "PAGE NOT FOUND"})}))))
--- src/gravie/backend/server.clj
+++ src/gravie/backend/server.clj
@@ -1,19 +1,13 @@
 (ns gravie.backend.server
   (:require [ring.adapter.jetty :as jetty]
-            [hiccup.page :as hp]))
+            [gravie.backend.routes :refer [routes]]))
 
 (defonce server (atom nil))
 
-(defn land-page [_]
-  {:status 200
-   :body (hp/html5
-          [:h1 "GiantBomb"]
-          [:div#root]
-          [:script {:src "/public/js/main.js"}])})
-
 (defn -main []
-  (reset! server (jetty/run-jetty #'land-page {:port 3000
-                                             :join? false})))
+  (reset! server (jetty/run-jetty #'routes
+                                  {:port 8080
+                                   :join? false})))
 
 
 
