a07b9d88899e39c7583e29d6ea36d8223c6fb429
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 13:43:58 -0300

    Add docstrings

--- src/gravie/frontend/subs.cljs
+++ src/gravie/frontend/subs.cljs
@@ -3,38 +3,45 @@
             [reagent-mui.icons.dark-mode :refer [dark-mode]]
             [reagent-mui.icons.light-mode :refer [light-mode]]))
 
+;;; check if checkout modal element open?
 (rf/reg-sub
  ::checkout-open?
  (fn [db]
    (-> db :ui :checkout :open?)))
 
+;;; return the cart
 (rf/reg-sub
  ::cart-items
  (fn [db]
    (:cart db)))
 
+;;; return cart count
 (rf/reg-sub
  ::cart-count
  :<- [::cart-items]
  (fn [items]
    (count items)))
 
+;;; return the search results
 (rf/reg-sub
  ::results
  (fn [db]
    (-> db :search-response :results)))
 
+;;; return mui theme
 (rf/reg-sub
  ::theme
  (fn [db]
    (:theme db)))
 
+;;; return mui theme mode
 (rf/reg-sub
  ::theme-mode
  :<- [::theme]
  (fn [theme]
    (get-in theme [:palette :mode])))
 
+;;; infer the mode icon from theme mode
 (rf/reg-sub
  ::mode-icon
  :<- [::theme-mode]
@@ -44,12 +51,13 @@
      "dark" light-mode
      :else dark-mode)))
 
-
+;;; get giantbomb logo or fallback
 (rf/reg-sub
  ::gb-logo
  (fn [db]
    (get-in db [:ui :logo :giantbomb])))
 
+;;; get gravie logo or fallback
 (rf/reg-sub
  ::gravie-logo
  (fn [db]
