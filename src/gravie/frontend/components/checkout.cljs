d007bffc72296e92926b75ef15077d5ffd5f142d
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 15:36:32 -0300

    Add buttons

--- src/gravie/frontend/components/checkout.cljs
+++ src/gravie/frontend/components/checkout.cljs
@@ -3,19 +3,25 @@
             [reagent-mui.material.dialog :refer [dialog]]
             [reagent-mui.material.dialog-title :refer [dialog-title]]
             [reagent-mui.material.dialog-content :refer [dialog-content]]
+            [reagent-mui.material.dialog-actions :refer [dialog-actions]]
             [reagent-mui.icons.delete-forever :refer [delete-forever]]
+            [reagent-mui.material.button :refer [button]]
             [reagent-mui.material.icon-button :refer [icon-button]]
             [reagent-mui.material.paper :refer [paper]]
             [reagent-mui.material.typography :refer [typography]]
-            [re-frame.core :as rf]
+            [gravie.frontend.utils :refer [<sub >evt]]
             [gravie.frontend.subs :as subs]
             [gravie.frontend.events.checkout :as checkout]
             [gravie.frontend.events.cart :as cart]))
 
+;;; does not work possible because dialog mui element
 (def checkout-styles
+  "Checkout styles map"
   {})
 
-(defn item-content [{:keys [name price image uuid]}]
+(defn item-content
+  "Items dialog contentent"
+  [{:keys [name price image uuid]}]
   [:div {:style {:display "flex"
                  :justify-content "space-between"
                  :width "100%"}}
@@ -32,30 +38,34 @@
      [typography name]
      [typography {:style {:margin-left "97px"}}
       (str "$ " price)]]]
-   [icon-button {:on-click #(rf/dispatch [::cart/delete-item uuid])} [delete-forever]]])
+   [icon-button {:on-click #(>evt [::cart/delete-item uuid])} [delete-forever]]])
 
-(defn checkout-dialog* [{:keys [class-name]}]
-  (let [open? (rf/subscribe [::subs/checkout-open?])
-        on-close-fn #(rf/dispatch [::checkout/close])
-        items (rf/subscribe [::subs/cart-items])
-        total (fn [items]
-                (->> items
-                     (map :price)
-                     (map #(js/parseFloat %))
-                     (reduce +)))]
-    (fn []
-      [:div {:class class-name}
-       [dialog {:open @open?
-                :on-close on-close-fn
-                :scroll :paper}
-        [dialog-title 
-         [typography (str "You selected " (count @items) " items")]
-         [typography (str "Total Cost: $" (.toFixed (total @items) 2))]]
-        [dialog-content {:dividers true}
-         (if (empty? @items)
-           [typography "NO ITEMS TO RENT"]
-           (for [item @items]
-             ^{:key (:uuid item)}
-             [item-content item]))]]])))
+(defn checkout-dialog*
+  "Reagent checkout"
+  [{:keys [class-name]}]
+  (let [open? (<sub [::subs/checkout-open?])
+        on-close-fn #(>evt [::checkout/close])
+        items (<sub [::subs/cart-items])
+        items-count (<sub [::subs/cart-count])
+        total (<sub [::subs/cart-total-price])]
+    [:div {:class class-name}
+     [dialog {:open open?
+              :on-close on-close-fn
+              :scroll :paper}
+      [dialog-title 
+       [typography (str "You have " items-count " items in cart")]
+       [typography (str "Total Cost: $" total)]]
+      [dialog-content {:dividers true}
+       (if (empty? items)
+         [typography "NO ITEMS TO RENT"]
+         (for [item items]
+           ^{:key (:uuid item)}
+           [item-content item]))]
+      [dialog-actions
+       [button {:on-click on-close-fn} "Rent more!"]
+       [button {:variant "outlined"
+                :color "success"} "Checkout"]]]]))
 
-(def checkout-dialog (styled checkout-dialog* checkout-styles))
+(def checkout-dialog
+  "Styled checkout"
+  (styled checkout-dialog* checkout-styles))
