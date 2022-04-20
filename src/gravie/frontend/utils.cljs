3684937a2639d421cabdf2c39fa1f13278aae241
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 13:43:47 -0300

    Create >evt helper fn

--- src/gravie/frontend/utils.cljs
+++ src/gravie/frontend/utils.cljs
@@ -1,7 +1,11 @@
 (ns gravie.frontend.utils
   (:require [re-frame.core :as rf]))
 
+;;; LIN (lambdaisland naming) :)
+;;; https://lambdaisland.com/blog/11-02-2017-re-frame-form-1-subscriptions
+
 (def <sub (comp deref rf/subscribe))
+(def >evt rf/dispatch)
 
 (defn get-key
   "Keycode helper"
