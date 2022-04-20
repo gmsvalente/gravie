2aaa30b1ec632ba8ca93444c7d3697e04be78cce
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 18:25:44 -0300

    Check for games not-found

--- src/gravie/frontend/components/result_view.cljs
+++ src/gravie/frontend/components/result_view.cljs
@@ -1,6 +1,7 @@
 (ns gravie.frontend.components.result-view
   (:require [reagent-mui.styles :refer [styled]]
             [reagent-mui.material.grid :refer [grid]]
+            [reagent-mui.material.typography :refer [typography]]
             [gravie.frontend.components.game-view :refer [game-view]]
             [gravie.frontend.utils :refer [<sub]]
             [gravie.frontend.subs :as subs]))
@@ -15,14 +16,17 @@
 (defn result-view*
   "Reagent result-view"
   [{:keys [class-name]}]
-  [:div {:class class-name}
-   [grid {:class "results"
-          :container true}
-    (for [game (<sub [::subs/results])]
-      ^{:key (:id game)}
-      [grid {:item true
-             :md 4 :sm 6 :xs 12}
-       [game-view {:game game}]])]])
+  (let [games (<sub [::subs/results])]
+    [:div {:class class-name}
+     [grid {:class "results"
+            :container true}
+      (if (empty? games)
+        [typography "No games found"]
+        (for [game games]
+          ^{:key (:id game)}
+          [grid {:item true
+                 :md 4 :sm 6 :xs 12}
+           [game-view {:game game}]]))]]))
 
 
 (def result-view
