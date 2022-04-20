cb4c160382acd22680eb96a57aa2e64eaa8797c2
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 12:46:12 -0300

    Fix body tag to html

--- src/gravie/backend/handlers.clj
+++ src/gravie/backend/handlers.clj
@@ -4,18 +4,20 @@
 
 (defn land-page [_]
   {:status 200
-   :body (hp/html5
+   :body (hp/html5 {}
           [:head
            [:meta {:name "viewport"
                    :content "width=device-width, initial-scale=1.0"}]
            [:meta {:charset "utf-8"}]]
-          [:div#root]
-          [:script {:src "/js/main.js"}])})
+          [:body
+           [:div#root]
+           [:script {:src "/js/main.js"}]])})
 
 (defn search [{:keys [query-params] :as req}]
   {:status 200
+   :headers {"Content-Type" "application/json"}
    :body (search-gb query-params)})
 
 (defn checkout [req]
     {:status 200
-   :body "checkout"})
+     :body "checkout"})
