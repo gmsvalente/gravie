a6dfd9cfd4fd8a33f4de18977189c96f03a62cfd
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 18:05:37 -0300

    More docstrings

--- src/gravie/frontend/events/cart.cljs
+++ src/gravie/frontend/events/cart.cljs
@@ -1,12 +1,13 @@
 (ns gravie.frontend.events.cart
   (:require [re-frame.core :as rf]))
 
+;;; add game to cart
 (rf/reg-event-db
  ::add-to-cart
  (fn [db [_ game]]
    (update db :cart conj game )))
 
-
+;;; delete item in cart
 (rf/reg-event-db
  ::delete-item
  (fn [db [_ item-uuid]]
--- src/gravie/frontend/events/checkout.cljs
+++ src/gravie/frontend/events/checkout.cljs
@@ -1,11 +1,13 @@
 (ns gravie.frontend.events.checkout
   (:require [re-frame.core :as rf]))
 
+;;; close checkout dialog
 (rf/reg-event-db
  ::close
  (fn [db _]
    (assoc-in db [:ui :checkout :open?] false)))
 
+;;; open checkout dialog
 (rf/reg-event-db
  ::open
  (fn [db _]
