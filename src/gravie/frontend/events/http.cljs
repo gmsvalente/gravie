5306da99b8661a1289095d17bad4f5634a957412
gmsvalente <gustavomsvalente@gmail.com>
2022-04-20 23:32:05 -0300

    Create checkout http request event

--- src/gravie/frontend/events/http.cljs
+++ src/gravie/frontend/events/http.cljs
@@ -1,7 +1,9 @@
 (ns gravie.frontend.events.http
   (:require [ajax.core :as ajax]
             [day8.re-frame.http-fx]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [gravie.frontend.events.cart :as cart]
+            [gravie.frontend.events.checkout :as checkout]))
 
 ;;; on sucess-get insert app-db search-response
 (rf/reg-event-db
@@ -27,3 +29,30 @@
                  :response-format (ajax/json-response-format  {:keywords? true})
                  :on-success [::success-get]
                  :on-failure [::error-get]}}))
+
+
+;;; on sucess-post
+(rf/reg-event-fx
+ ::success-post
+ (fn [{:keys [db]} [_ response]]
+   {:db (assoc db :post-response response)
+    :fx [[:dispatch [::checkout/close]]
+         [:dispatch [::cart/clear-cart]]]}))
+
+;;; on error-post
+(rf/reg-event-db
+ ::error-post
+ (fn [db [_ response]]
+   (assoc db :post-error-response response)))
+
+;;; http server post 
+(rf/reg-event-fx
+ ::request-post
+  (fn [_ [_ cart]]
+   {:http-xhrio {:method :post
+                 :uri "/checkout"
+                 :params {:data cart}
+                 :format (ajax/json-request-format )
+                 :response-format (ajax/json-response-format  {:keywords? true})
+                 :on-success [::success-post]
+                 :on-failure [::error-post]}}))
