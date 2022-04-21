f7259047809f5220055c078df42de70dcf78e712
gmsvalente <gustavomsvalente@gmail.com>
2022-04-21 01:31:03 -0300

    Redefine game-view layout

--- src/gravie/frontend/components/game_view.cljs
+++ src/gravie/frontend/components/game_view.cljs
@@ -8,9 +8,8 @@
             [reagent-mui.material.typography :refer [typography]]
             [reagent-mui.material.fab :refer [fab]]
             [reagent-mui.icons.add-shopping-cart :refer [add-shopping-cart]]
-            [gravie.frontend.utils :refer [>evt <sub]]
-            [gravie.frontend.events.cart :as cart]
-            [gravie.frontend.subs :as subs]))
+            [gravie.frontend.utils :refer [>evt]]
+            [gravie.frontend.events.cart :as cart]))
 
 (defn show-platforms
   "Show platforms helper fn"
@@ -22,29 +21,30 @@
 (defn game-view-styles
   "Game-view styles map"
   [{:keys [theme]}]
-  {".info" {:display "flex"
-            :margin "7px"
-            :min-height "200px"
-            :justify-content "space-around"
-            :align-items "center"
-            :color "black"
-            :background-color "silver"}
-   ".rent-box" {:display "flex"
-                :flex-direction "column"
-                :min-height "150px"
-                :max-height "200px"
-                :justify-content "space-around"
-                :align-items "center"}
-   ".card" {:margin "10px"
+  {".card" {:margin "10px"
             :background (-> theme :palette :primary :dark)
-            :min-height "350px"}
-   ".platforms" {:margin-top "7px"
-                 :max-height "200px"
-                 :overflow "auto"}
+            :height "646px"}
    ".card-header" {:background (-> theme :palette :primary :dark)
+                   :height "10%"
                    :color "black"}
    ".card-content" {:background (-> theme :palette :primary :light)
-                    :color "black"}})
+                    :overflow "auto"
+                    :height "28%"
+                    :color "black"}
+   ".info" {:display "flex"
+            :height "30%"
+            :align-items "flex-start"
+            :color "black"
+            :background-color "silver"}
+   ".platforms-box" {:margin "3px"
+                     :width "60%"
+                     :height "200px"
+                     :overflow "auto"}
+   ".rent-box" {:display "flex"
+                :width "40%"
+                :flex-direction "column"
+                :justify-content "center"
+                :align-items "center"}})
 
 (defn game-view*
   "Reagent game-view"
@@ -61,10 +61,12 @@
         [card-header {:title name
                       :class "card-header"}]
         [card-media {:component "img"
-                     :image (:medium-url image)}]
+                     :image (:medium-url image)
+                     :height "40%"
+                     :class ".card-image"}]
         [card-content {:class "card-content"} deck]
         [paper {:class "info"}
-         [card-content
+         [card-content {:class "platforms-box"}
           [typography "Platforms:"]
           [show-platforms platforms]]
          [card-content {:class "rent-box"}
